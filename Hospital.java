48c3420cc9eaa9094be59e2ac8744c5d43736712
Abdulsamet Dursun <46134955+Abdulsametdursun@users.noreply.github.com>
2020-11-23 19:45:34 -0600

    Add files via upload

---
+++ Hospital.java
@@ -0,0 +1,30 @@
+public class Hospital{
+  
+  public static void main(String[] args){
+    
+    HospitalEmployee vito = new HospitalEmployee ("Vito", 123);
+    Doctor michael = new Doctor ("Michael", 234, "Heart");
+    Surgeon vincent = new Surgeon ("Vincent", 645, "Brain", true);
+    Nurse sonny = new Nurse ("Sonny", 789, 6);
+    Administrator luca = new Administrator ("Luca", 375, "Business");
+    Receptionist tom = new Receptionist ("Tom", 951, "Talking", true);
+    Janitor anthony = new Janitor ("Anthony", 123, "Maintenence", false);
+    
+    System.out.println (vito);
+    System.out.println (michael);
+    System.out.println (vincent);
+    System.out.println (sonny);
+    System.out.println (luca);
+    System.out.println (tom);
+    System.out.println (anthony);
+    
+    vito.work();
+    michael.diagnose();
+    vincent.operate();
+    sonny.assist();
+    luca.administrate();
+    tom.answer();
+    anthony.sweep();
+    
+  }
+}
